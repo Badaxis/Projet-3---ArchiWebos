312cf5fc98043731160455936dd6502598061657
Badaxis <vingtsang.billet@gmail.com>
2024-06-05 11:04:02 +0200

    Projet complété

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,13 +1,13 @@
 const form = document.querySelector("#login-form");
-form.addEventListener("submit", async event => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const email = document.querySelector("#email").value;
   const password = document.querySelector(["#pwd"]).value;
-  const pwdErr = document.querySelector("#pwd-error");
-  pwdErr.textContent = "";
   const emailErr = document.querySelector("#email-error");
-  emailErr.textContent = "";
+  const pwdErr = document.querySelector("#pwd-error");
+  emailErr.classList.add("hidden");
+  pwdErr.classList.add("hidden");
 
   const reponse = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
@@ -16,15 +16,14 @@ form.addEventListener("submit", async event => {
   });
 
   const result = await reponse.json();
-
   if (reponse.ok) {
     window.sessionStorage.setItem("token", result.token);
     window.location.href = "./index.html";
   } else {
-    if (reponse.status === 401) {
-      pwdErr.textContent = "Mot de passe incorrect";
+    if (reponse.status === 404) {
+      emailErr.classList.remove("hidden");
     } else {
-      emailErr.textContent = "Utilisateur inconnu";
+      pwdErr.classList.remove("hidden");
     }
   }
 });
--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,12 +1,14 @@
 const projets = await fetch("http://localhost:5678/api/works").then(proj => proj.json());
 
+const token = window.sessionStorage.getItem("token");
+
 const categories = ["Tous"];
 
 // Création de la gallerie
-const genererGallerie = projets => {
+const gallery = document.querySelector(".gallery");
+const genererGallerie = (projets) => {
+  gallery.innerHTML = "";
   for (let projet of projets) {
-    const gallery = document.querySelector(".gallery");
-
     const figureElement = document.createElement("figure");
     const imgElement = document.createElement("img");
     imgElement.src = projet.imageUrl;
@@ -14,8 +16,10 @@ const genererGallerie = projets => {
     const figcaptionElement = document.createElement("figcaption");
     figcaptionElement.innerText = projet.title;
 
-    const categorie = projet.category.name;
-    categories.push(categorie);
+    if (projet.category) {
+      const categorie = projet.category.name;
+      categories.push(categorie);
+    }
 
     gallery.appendChild(figureElement);
     figureElement.appendChild(imgElement);
@@ -26,9 +30,8 @@ genererGallerie(projets);
 
 // Création du menu de filtres
 const setCategories = new Set(categories);
+const filtres = document.querySelector(".filtres");
 for (let categorie of setCategories) {
-  const filtres = document.querySelector(".filtres");
-
   const buttonElement = document.createElement("button");
   buttonElement.textContent = categorie;
   buttonElement.className = categorie === "Tous" ? "btn-filter-active" : "btn-filter-inactive";
@@ -42,31 +45,164 @@ for (let categorie of setCategories) {
       document.querySelector(".btn-filter-active").className = "btn-filter-inactive";
       buttonElement.className = "btn-filter-active";
     }
-    document.querySelector(".gallery").innerHTML = "";
     genererGallerie(projetsFiltres);
   });
-
   filtres.appendChild(buttonElement);
 }
 
-const adminPage = () => {
+// Fonctionnalités de la modale
+const modal = document.querySelector("dialog");
+
+const validerBtn = document.querySelector("#valider-btn");
+const filePicker = document.querySelector("#add-btn");
+const selectCategorie = document.querySelector("#form-categorie");
+const newProjTitle = document.querySelector("#form-titre");
+const validerRequired = [filePicker, selectCategorie, newProjTitle];
+const checkRequired = () => {
+  let allFilled = true;
+  validerRequired.forEach((input) => {
+    if (!input.value.trim() || input.value == 0) {
+      allFilled = false;
+    }
+  });
+  validerBtn.disabled = !allFilled;
+};
+
+const photoSelect = document.querySelector(".photo-select");
+const preview = document.querySelector(".photo-preview");
+filePicker.addEventListener("change", () => {
+  const file = filePicker.files[0];
+  const photo = document.createElement("img");
+  photo.src = URL.createObjectURL(file);
+  preview.appendChild(photo);
+  preview.setAttribute("class", "photo-preview");
+  photoSelect.setAttribute("class", "hidden");
+  checkRequired();
+});
+newProjTitle.addEventListener("input", () => checkRequired());
+selectCategorie.addEventListener("input", () => checkRequired());
+
+const modalSwitch = () => {
+  const deleteSection = document.querySelector(".modal-delete");
+  deleteSection.classList.toggle("hidden");
+  const addSection = document.querySelector(".modal-add");
+  addSection.classList.toggle("hidden");
+  const returnBtn = document.querySelector(".return-btn");
+  returnBtn.classList.toggle("hidden");
+};
+
+const closeModal = () => {
+  modal.close();
+  if (preview.hasChildNodes()) {
+    preview.removeChild(preview.firstChild);
+  }
+  preview.setAttribute("class", "hidden");
+  photoSelect.setAttribute("class", "photo-select");
+  validerRequired.forEach((input) => input.value = "");
+  validerBtn.disabled = true;
+  if (document.querySelector(".modal-delete").classList.contains("hidden")) {
+    modalSwitch();
+  }
+};
+closeModal();
+
+const modalForm = document.querySelector("#modal-form");
+modalForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(modalForm);
+  const reponse = await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: { "Authorization": `bearer ${token}` },
+    body: formData
+  });
+  if (reponse.ok) {
+    closeModal();
+    const result = await reponse.json();
+    projets.push(result);
+    genererGallerie(projets);
+  } else {
+    console.error(reponse.status);
+  }
+});
+
+modal.addEventListener("click", (event) => {
+  const rect = modal.getBoundingClientRect();
+  if ((event.clientX < rect.left || event.clientX > rect.right
+    || event.clientY < rect.top || event.clientY > rect.bottom)
+    && event.target === modal
+  ) { closeModal(); }
+});
+
+const closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", () => closeModal());
+const returnBtn = document.querySelector(".return-btn");
+returnBtn.addEventListener("click", () => modalSwitch());
+const ajoutBtn = document.querySelector("#ajout-btn");
+ajoutBtn.addEventListener("click", () => modalSwitch());
+
+const modalCategories = [...setCategories];
+modalCategories[0] = "";
+for (let modalCategorie in modalCategories) {
+  const optElement = document.createElement("option");
+  optElement.value = modalCategorie;
+  optElement.text = modalCategories[modalCategorie];
+  selectCategorie.appendChild(optElement);
+}
+
+const generateModalGallery = () => {
+  const modalGallery = document.querySelector(".modal-gallery");
+  modalGallery.innerHTML = "";
+  for (let projet of projets) {
+    const figureElement = document.createElement("figure");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can fa-xs"></i>`;
+    deleteBtn.className = "delete-btn";
+    const imgElement = document.createElement("img");
+    imgElement.src = projet.imageUrl;
+    imgElement.alt = projet.title;
+    deleteBtn.addEventListener("click", async (event) => {
+      event.preventDefault();
+      const imageId = projet.id;
+      const reponse = await fetch(`http://localhost:5678/api/works/${imageId}`, {
+        method: "DELETE",
+        headers: { "Authorization": `bearer ${token}` }
+      });
+      if (reponse.ok) {
+        const projIndex = projets.findIndex(proj => proj.id === imageId);
+        projets.splice(projIndex, 1);
+        genererGallerie(projets);
+        generateModalGallery();
+      } else {
+        console.error(reponse.status);
+      }
+    });
+    modalGallery.appendChild(figureElement);
+    figureElement.appendChild(deleteBtn);
+    figureElement.appendChild(imgElement);
+  }
+};
+
+// Version admin après connexion
+const adminPageSetup = () => {
   const banniere = document.querySelector(".banniere");
   banniere.classList.toggle("hidden");
+  const header = document.querySelector("header");
+  header.setAttribute("class", "header-admin");
   const loginLink = document.querySelector(".login-link");
   loginLink.setAttribute("href", "./index.html");
-  loginLink.innerHTML = "<li>logout</li>";
+  loginLink.textContent = "logout";
   loginLink.addEventListener("click", () => {
     window.sessionStorage.removeItem("token");
   });
   const modifBouton = document.querySelector(".modifier-btn");
   modifBouton.classList.toggle("hidden");
-  const filtres = document.querySelector(".filtres");
+  modifBouton.addEventListener("click", () => {
+    generateModalGallery();
+    modal.showModal();
+  });
   filtres.setAttribute("class", "hidden");
 };
 
-const token = window.sessionStorage.getItem("token");
 if (token) {
-  adminPage();
-} else {
-  console.log("No token");
+  adminPageSetup();
 }
